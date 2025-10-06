14b2cc8c311558a354b599c3a7373fb5f04e84ff
Diep Thanh Phuong <diepthanhphuong0407@gmail.com>
2025-10-06 15:44:23 +0200

    moving main to main.c

--- airfoil_generator.c
+++ airfoil_generator.c
@@ -70,37 +70,3 @@ void plot_airfoil(const char* filename) {
     
     fflush(gnuplotPipe);
 }
-
-int main() {
-    int N = 100;
-    int m = 2, p = 4, t = 15;
-    double c = 1.0;
-    Airfoil_t af;
-
-    generate_airfoil_4digit(m, p, t, c, N, &af);
-
-    FILE *fp = fopen("naca2412.dat", "w");
-    if (!fp) {
-        perror("Cannot open file");
-        return 1;
-    }
-
-    for (int i = 0; i <= N; i++) {
-        fprintf(fp, "%f %f\n", af.x_u[i], af.y_u[i]);
-    }
-    for (int i = N - 1; i >= 0; i--) {
-        fprintf(fp, "%f %f\n", af.x_l[i], af.y_l[i]);
-    }
-
-    fclose(fp);
-
-    printf("Data exported to file.dat\n");
-
-    plot_airfoil("naca2412.dat");
-
-    free(af.x_c); free(af.y_c);
-    free(af.x_u); free(af.y_u);
-    free(af.x_l); free(af.y_l);
-
-    return 0;
-}
